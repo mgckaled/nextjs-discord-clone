6fbbe7fbefe060feecdf6896fb90bb1e714cb54d
mgckaled <marcelgckaled@gmail.com>
2023-09-07 21:11:38 -0300

    chore: setup some files and folders

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,18 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next"
 import { Open_Sans } from "next/font/google"
-import './globals.css'
+import "./globals.css"
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Discord",
   description: "Fullstack Discord Clone with Next.js 13, React, Socket.io, Prisma, Tailwind, MySQL",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={openSans.className}>{children}</body>
     </html>
   )
 }
-
-
